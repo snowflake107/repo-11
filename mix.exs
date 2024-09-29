4e7781a5ae7f367399ad0c689457be739f885f43
Hew <github@mhs.fastmail.com>
2024-09-28 23:14:08 -0400

    Bump minimum Elixir version to match req

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Wpcom.MixProject do
     [
       app: :wpcom,
       version: "1.0.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
